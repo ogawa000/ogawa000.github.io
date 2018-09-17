9ed79b82a380810954cf768f8bfc7e1ee5fdf4e5
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2018-09-17 14:58:41 +0900

    test

---
+++ index.js
@@ -0,0 +1,161 @@
+function initialseStatus(){
+    if(!('showNotification' in ServiceWorkerRegistration.prototype)){
+        console.log('プッシュ通知が対応されておりません');
+        return;
+    }
+    if(Notification.permission === 'denied'){
+        console.log('通知をブロックしております');
+        return;
+    }
+    if(!('PushManager' in window)){
+        console.log('プッシュ通知が対応されておりません');
+        return;
+    }
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration){
+        serviceWorkerRegistration.pushManager.getSubscription().then(
+            function(subscription){
+                pushEnableButton.disabled = false;
+                pushDisableButton.disabled = false;
+                
+                pushEnableButton.classList.add('non-active');
+                pushEnableButton.disabled = false;
+                
+                pushDisableButton.classList.remove('non-active');
+                pushDisablebutton.disabled = true;
+                
+                if(!subscription){
+                    return;
+                }
+                
+                //取得したsubscriptionをサーバなどで保存っせる処理
+                sendSubscriptionToServer(subscription);
+                
+                pushEnableButton.classList.remove('non-active');
+                pushEnableButton.disabled = true;
+                
+                pushDisableButton.classList.add('non-active');
+                pushDisableButton.disabled = false;
+            }
+       )
+        .catch(function(err){console.warn('Error during getSubscription',err);});
+    });
+}
+
+if('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)
+    ){
+        navigator.serviceWorker.register('serviceWorker.js',{scope:'/'}).then(
+            function(registration){
+                if(typeof registration.update == 'function'){
+                    registration.update();
+                }
+                initialseStatus();
+            }
+        )
+        .catch(function(error){
+            console.error('Service Worker registration failed:',error);
+        });
+    }
+
+pushEnableButton.addEventListener('click',function(){
+    Notification.requestPermission(function(permission){
+        if(permission !== 'denied'){
+            subscribe();
+        }
+        else {
+            alert('プッシュ通知に対応を有効にできません。ブラウザの設定を確認してください。');
+        }
+        
+    });
+});
+
+function subscribe(){
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration){
+       serviceWorkerRegistration.pushManager.subscribe({userVisbleOnly:true}).then(
+           function(subscription){
+               pushEnableButton.classList.remove('non-active');
+               pushEnableButton.disabled = true;
+               
+               pushDisableButton.classList.add('non-active');
+               pushDisableButton.disabled = false;
+               
+               return sendSubscriptionToServer(subscription);
+           }
+       ) 
+        .catch(
+            function(e){
+                if(Notification.permission == 'denied'){
+                    console.warn('permission for notification was denied');
+                }
+                else{
+                    console.error('unable to subscribe to push',e);
+                    window.alert(e);
+                }
+            }
+       )
+    });
+}
+
+function sendSubscribeToServer(subscription){
+    console.log(subscription);
+}
+
+pushDisableButton.addEventListener('click',function(){
+    unsubscribed();
+});
+
+function unsubscribed(){
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration){
+        serviceWorkerRegistration.pushManager.getSubscription().then(
+            function(pushSubscription){
+                if(!pushSubscription){
+                    pushEnableButton.classList.add('non-active');
+                    pushEnableButton.disabled = false;
+                    
+                    pushDisableButton.classList.remove('non-active');
+                    pushDisableButton.disabled = true;
+                    
+                    return;
+                }
+                
+                sendSubscribeToServerForDelete(pushSubscription);
+                
+                pushSubscription.unsubscribe().then(function(successful){
+                    pushEnableButton.classList.add('non-active');
+                    pushEnableButton.disabled = false;
+                    
+                    pushDisableButton.classList.remove('non-active');
+                    pushDisableButton.disabled = true;
+                }).catch(function(e){
+                    console.error('unsubscription error:',e);
+                    
+                    pushEnableButton.classList.add('non-active');
+                    pushEnableButton.disabled = false;
+                    
+                    pushDisableButton.classList.remove('non-active');
+                    pushDisableButton.disabled = true;
+                });
+            }
+        )
+        .catch(
+            function(e){
+                console.error('error thrown while unsubscribing frome push messaging.',e);
+            }
+        )
+    });
+}
+
+funtion sendSubscribeToServerForDelete(subscription){
+    console.log('sending to server for delete:',subscription);
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,46 @@ self.addEventListener('active',function(e){
 
 //念仏
 self.addEventListener('fetch',function(event){});
+
+self.addEventListener('push',function(event){
+    self.addEventListener('push',function(event){
+        var title = 'メッセージが届きました';
+        var body = 'メッセージ内容はこんにちわ';
+        // var icon =
+        var tag = 'simple-push-demo-notification-tag';
+        
+        event.waitUntil(
+        
+            self.registration.showNotification(title,{
+                body:body,
+                tag:tag
+            })
+        );
+    });
+});
+
+self.addEventListener('notificationclick',function(event){
+    event.notification.close();
+    
+    event.waitUntil(
+        clients.matchAll({
+            type:"window"
+        })
+        .then(function(clientList){
+            for(var i =0;i<clientList.length;i++){
+                var client = clientList[i]:
+                if(client.url == '/' && 'focus' in client)
+                    return client.focus();
+            }
+            if(clients.openWindow){
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
+
+
+
+
+
+
