0261eaee19bb55e0aa8608b8b7e155bfcde7a438
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2018-09-17 16:51:21 +0900

    やり直し

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,11 @@
-self.addEventListener('push', function(event) {
-    self.addEventListener('push', function(event) {
-        var title = 'メッセージが届きました';
-        var body  = 'メッセージ内容は、こんにちわ！です';
-        //var icon  = '/images/touch/chrome-touch-icon-192x192.png';
-        var tag   = 'simple-push-demo-notification-tag';
-
-        event.waitUntil(
-            self.registration.showNotification(title, {
-                body: body,
-                icon: icon,
-                tag: tag
-            })
-        );
-    });
+// service-worker.js
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
 });
 
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-
-    event.waitUntil(
-        clients.matchAll({
-            type: "window"
-        })
-            .then(function(clientList) {
-                      for (var i = 0; i < clientList.length; i++) {
-                          var client = clientList[i];
-                          if (client.url == '/' && 'focus' in client)
-                              return client.focus();
-                      }
-                      if (clients.openWindow) {
-                          return clients.openWindow('/');
-                      }
-                  })
-    );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 });
+
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', function(event) {});
