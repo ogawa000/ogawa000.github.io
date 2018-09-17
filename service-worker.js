321ecce50100b9dd1e5237a219301d93298137c2
ogawa000 <43328782+ogawa000@users.noreply.github.com>
2018-09-17 16:55:14 +0900

    Revert "やり直し"
    This reverts commit 0261eaee19bb55e0aa8608b8b7e155bfcde7a438.

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,36 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+self.addEventListener('push', function(event) {
+    self.addEventListener('push', function(event) {
+        var title = 'メッセージが届きました';
+        var body  = 'メッセージ内容は、こんにちわ！です';
+        //var icon  = '/images/touch/chrome-touch-icon-192x192.png';
+        var tag   = 'simple-push-demo-notification-tag';
+
+        event.waitUntil(
+            self.registration.showNotification(title, {
+                body: body,
+                icon: icon,
+                tag: tag
+            })
+        );
+    });
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
 
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+            .then(function(clientList) {
+                      for (var i = 0; i < clientList.length; i++) {
+                          var client = clientList[i];
+                          if (client.url == '/' && 'focus' in client)
+                              return client.focus();
+                      }
+                      if (clients.openWindow) {
+                          return clients.openWindow('/');
+                      }
+                  })
+    );
+});
